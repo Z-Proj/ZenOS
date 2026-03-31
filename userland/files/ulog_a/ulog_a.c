7b14ee6d8936b6eb4c5b30976726351758850cfe
Rishies2010 <rishiewas2010@gmail.com>
2026-03-31 21:25:04 +0530

    intermediate Save 2

---
+++ userland/files/ulog_a/ulog_a.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include "../userlib.h"
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    pid_t pid = getpid();
+    char msg[96];
+
+    for (int i = 0; i < 8; i++)
+    {
+        snprintf(msg, sizeof(msg), "ulog_a pid=%d iter=%d", (int)pid, i);
+        zen_log(msg, 1, 1);
+        zen_sleep_ms(50);
+    }
+
+    snprintf(msg, sizeof(msg), "ulog_a pid=%d done", (int)pid);
+    zen_log(msg, 1, 1);
+    return 0;
+}
