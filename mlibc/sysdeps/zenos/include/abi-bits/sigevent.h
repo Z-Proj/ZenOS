ca8275be5c3398819cf117c42bf4cfdbb61e48a4
Rishies2010 <rishiewas2010@gmail.com>
2026-04-05 21:29:19 +0530

    clean: gitignore, blend2d, smth else

--- mlibc/sysdeps/zenos/include/abi-bits/sigevent.h
+++ mlibc/sysdeps/zenos/include/abi-bits/sigevent.h
@@ -1,1 +1,22 @@
-../../../../abis/zenos/sigevent.h
+#ifndef _ABIBITS_SIGEVENT_H
+#define _ABIBITS_SIGEVENT_H
+
+#include <abi-bits/pid_t.h>
+#include <abi-bits/sigval.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct sigevent {
+	int sigev_notify;
+	int sigev_signo;
+	union sigval sigev_value;
+	void (*sigev_notify_function)(union sigval);
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- userland/files/blend2d/blend2d-testing/demos/bl_qt_headers.h
+++
@@ -1,7 +0,0 @@
-#ifndef BL_QT_HEADERS_H_INCLUDED
-#define BL_QT_HEADERS_H_INCLUDED
-
-#include <QtGui>
-#include <QtWidgets>
-
-#endif // BL_QT_HEADERS_H_INCLUDED
